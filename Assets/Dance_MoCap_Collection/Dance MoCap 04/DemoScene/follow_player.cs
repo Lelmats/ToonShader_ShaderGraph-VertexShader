c684b0bde4932c558c1356f035c7eb71275528bb
Max Rivera <69810374+Lelmats@users.noreply.github.com>
2021-05-25 19:25:35 -0700

    Dancing and details

---
+++ Assets/Dance_MoCap_Collection/Dance MoCap 04/DemoScene/follow_player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+public class follow_player: MonoBehaviour
+{
+
+	public GameObject player;
+	public float cameraHeight = 10.0f;
+	public float cameraDistance = 5.0f; //distance from charecter
+
+
+	void Update() {
+
+		//set one
+		Vector3 pos = player.transform.position;
+
+		Debug.Log (pos.x);
+
+		pos.y = cameraHeight;
+		pos.x = cameraDistance + pos.x;
+		transform.position = pos;
+	}
+}
---
+++ Assets/Dance_MoCap_Collection/Dance MoCap 05/DemoScene/dance_mocap_05_follow_player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+public class dance_mocap_05_follow_player: MonoBehaviour
+{
+
+	public GameObject player;
+	public float cameraHeight = 10.0f;
+	public float cameraDistance = 5.0f; //distance from charecter
+
+
+	void Update() {
+
+		//set one
+		Vector3 pos = player.transform.position;
+
+		Debug.Log (pos.x);
+
+		pos.y = cameraHeight;
+		pos.x = cameraDistance + pos.x;
+		transform.position = pos;
+	}
+}
+
+
+
